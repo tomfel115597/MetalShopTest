54f26e0f77c0dcefb5518748619cba625582af95
tomfel115597 <tomasz.fellenberg@gmail.com>
2023-06-07 23:48:45 +0200

    modified of structure in tests in ShoppingTest

--- src/test/java/tests/ShoppingTest.java
+++ src/test/java/tests/ShoppingTest.java
@@ -3,7 +3,6 @@ package tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,22 +15,19 @@ public class ShoppingTest extends BaseTest {
     void checkCartIsEmptyAddProduct() {
         ShoppingPage shoppingPage = new ShoppingPage(driverChrome);
         shoppingPage.cart.click();
-        String cartText = "Twój koszyk aktualnie jest pusty.";
-        Assertions.assertEquals(cartText, shoppingPage.cartMessage.getText());
+
+        Assertions.assertEquals("Twój koszyk aktualnie jest pusty.", shoppingPage.cartMessage.getText());
 
         shoppingPage.homeSiteMenuItem.click();
 
         shoppingPage.productId24.click();
 
         Wait wait = new WebDriverWait(driverChrome, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.xpath("//a[@title='Zobacz koszyk']")));
+        wait.until(ExpectedConditions.visibilityOf(shoppingPage.showCart));
 
-        WebElement showCart = driverChrome.findElement(By.xpath("//a[@title='Zobacz koszyk']"));
-        showCart.click();
+        shoppingPage.showCart.click();
 
-        String productName = "Srebrna moneta 5g - UK 1980";
-        Assertions.assertEquals(productName, shoppingPage.showProduct.getText());
+        Assertions.assertEquals("Srebrna moneta 5g - UK 1980", shoppingPage.showProduct.getText());
     }
 
     @Test
